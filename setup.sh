18efaa45dc24f470314e403df7b63eeee245c830
Just Manny <mprancke@gmail.com>
2022-07-06 13:57:54 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release \
+    git
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
